c5bd0706ce929089277780a46bf1916dadeb5b75
Alex Patel <patelalex02@gmail.com>
2017-09-18 06:37:21 -0400

    drop in a merge sort implementation

--- src/sort/sort_good.py
+++ src/sort/sort_good.py
@@ -2,3 +2,31 @@
 #
 # Correct sort implementation
 #
+# Merge sort from http://danishmujeeb.com/blog/2014/01/basic-sorting-algorithms-implemented-in-python/
+#
+
+def merge_sort(items):
+    """ Implementation of mergesort """
+    if len(items) > 1:
+
+        mid = len(items) / 2        # Determine the midpoint and split
+        left = items[0:mid]
+        right = items[mid:]
+
+        merge_sort(left)            # Sort left list in-place
+        merge_sort(right)           # Sort right list in-place
+
+        l, r = 0, 0
+        for i in range(len(items)):     # Merging the left and right list
+
+            lval = left[l] if l &lt; len(left) else None
+            rval = right[r] if r &lt; len(right) else None
+
+            if (lval and rval and lval &lt; rval) or rval is None:
+                items[i] = lval
+                l += 1
+            elif (lval and rval and lval &gt;= rval) or lval is None:
+                items[i] = rval
+                r += 1
+            else:
+                raise Exception('Could not merge, sub arrays sizes do not match the main array')
